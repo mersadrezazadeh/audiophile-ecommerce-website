1014fafb7d0677b72f1909fce784561f2f2391f6
mersadrezazadeh <mersadrezazadeh@gmail.com>
2024-01-27 19:48:23 +0330

    add container

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,15 +4,17 @@ import NavLinks from "./NavLinks";
 
 function Navbar() {
   return (
-    <header className="relative z-50 flex items-center justify-between border-b border-b-gray-300/20 bg-gray-800 px-4 py-8">
-      <Logo />
-      <NavLinks />
-      <div className="xl:hidden" />
-      <div className="xl:hidden" />
-      <div className="xl:hidden" />
-      <div className="xl:hidden" />
-      <div className="xl:hidden" />
-      <CartPreview />
+    <header className="relative z-50  bg-gray-800">
+      <div className="container flex items-center justify-between border-b border-b-gray-300/20 px-4 py-8">
+        <Logo />
+        <NavLinks />
+        <div className="xl:hidden" />
+        <div className="xl:hidden" />
+        <div className="xl:hidden" />
+        <div className="xl:hidden" />
+        <div className="xl:hidden" />
+        <CartPreview />
+      </div>
     </header>
   );
 }
